dc82673519a80d293bceb92d2f260ebe2b6f097d
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-05-07 17:03:37 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,10 @@ end
 def monopoly_with_third_tier
 	monopoly = {
 		:railroads =>
-		{ :pieces => 4,
+		{ :pieces =>4,
 			:rent_in_dollars =>
 			{
-				:one_piece_owned => 25,
+				:one_piece_owned =>25,
 			},
 
 		 :names => {}, :rent_in_dollars => {}
